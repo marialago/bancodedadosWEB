f8569d283552850dbd8a7000243dffb20748313e
Maria Lago <50707572+marialago@users.noreply.github.com>
2021-12-18 20:49:37 -0300

    Update index.php

--- unidade 1/index.php
+++ unidade 1/index.php
@@ -2,19 +2,23 @@
 <html>
 
 <head>
-	<title>Loja de Anoes do Pedroca</title>
+	<title>BDWeb - Página Inicial</title>
 </head>
 
 <body>
 
-	<h1>Anoes para todos os gostos</h1>
-	<p>Garanta ja seu anao com o maior custo beneficio do mercado</p>
-
-    <a href = "cadastro.php">Cadastro</a> </br>
-    <a href = "login.php">Login</a> 
-    <br>
-
+	<h1>Banco de dados para WEB</h1>
+	<p> Página criada com o intuito de estudar os conteudos iniciais para o desenvolvimento de um banco de dados utilizando PHP, html e SQL</p>
         
-    </body>
+        
+        <a href="login.php">Login<br></a>
+        <a href="cadastro.php">Cadastro<br></a>
+
+<footer>
+    <p>SISTEMA BDWeb</p>
+    <p> Author: Maria Eduarda Fernandes Lago<br>
+        E-mail: <a href = "marialagoufrn@gmail.com">marialagoufrn@gmail.com</a></p>
+</footer>
+
 
 </html>
